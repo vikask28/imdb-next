d60bd13db53b5776d74cf1c49b0ce4795b5d7402
Vikas Kumar <155806172+vikask28@users.noreply.github.com>
2024-03-09 15:26:03 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4 text-justify'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione voluptatum asperiores voluptatem repudiandae quasi blanditiis inventore soluta possimus, tempore voluptatibus rem est? Laboriosam ad quas soluta consectetur alias accusantium nisi?
+        Vel eaque minus nostrum totam eos optio praesentium pariatur quod ipsa? Sapiente velit quos eligendi consequuntur, temporibus vitae, nemo aperiam officia deserunt iure sint, maxime modi harum ipsam eos non!
+        Aperiam quibusdam reprehenderit, laudantium, veniam nesciunt aliquid id esse beatae nam a ad sint quasi recusandae necessitatibus maiores! Nam, ad voluptas nemo mollitia sed laudantium minima aspernatur suscipit unde sapiente? Repudiandae numquam earum quo animi officiis voluptas.
+        </p>
+        <p>
+        Repudiandae repellat fugit dolorum aperiam fugiat minus delectus sunt quas, assumenda veniam. Quidem accusamus iste commodi vero atque repellendus. Ducimus maiores quas, et repellat saepe commodi adipisci consectetur. Excepturi, recusandae!
+        </p>
+    </div>
+  )
+}
+
+export default About
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <div className='flex justify-between items-center p-3 lg:max-w-6xl mx-auto'>
 
-        <div className="flex gap-4">
+        <div className="flex gap-14 text-xl">
             <MenuItem title="home" address="/" Icon={AiFillHome}/>
             <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill}/>
         </div>
